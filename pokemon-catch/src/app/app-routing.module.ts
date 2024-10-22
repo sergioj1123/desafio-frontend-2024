35f5a3b6c2b32bd7b78b88f6e6be37103dc32070
Sergio Felix <sergiofelix9@gmail.com>
2024-10-22 14:31:59 -0300

    feat: add rote and component rote for weather component

--- pokemon-catch/src/app/app-routing.module.ts
+++ pokemon-catch/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormWeatherComponent } from './weather/form-weather/form-weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'weather',
+    pathMatch: 'full',
+  },
+  {
+    path: 'weather',
+    redirectTo: 'weather',
+  },
+  {
+    path: 'weather',
+    component: FormWeatherComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
