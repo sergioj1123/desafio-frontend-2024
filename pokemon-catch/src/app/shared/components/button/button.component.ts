00372d5eccae895b9507e1ff4c2a6dfbf55f1ec8
Sergio Felix <sergiofelix9@gmail.com>
2024-10-23 16:39:23 -0300

    feat: Add button component

---
+++ pokemon-catch/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'button-component',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() dataFromParent!: string;
+}
