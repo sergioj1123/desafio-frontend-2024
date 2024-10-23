3a98ae0228b2359d3b20b477bad68ecea5376268
Sergio Felix <sergiofelix9@gmail.com>
2024-10-23 18:44:49 -0300

    feat: module to pokemon page

---
+++ pokemon-catch/src/app/modules/pokemon/pokemon.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonInfoComponent } from './pokemon-info/pokemon-info.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokemonInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokemonModule { }
