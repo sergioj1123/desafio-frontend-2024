d6f4cab776c171481c4734e0da57a249f3eaf9fe
Sergio Felix <sergiofelix9@gmail.com>
2024-10-23 16:44:46 -0300

    feat: add the component city information as child of form-weather component

--- pokemon-catch/src/app/weather/form-weather/form-weather.component.ts
+++ pokemon-catch/src/app/weather/form-weather/form-weather.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Weather } from '../../shared/models/weather.model';
 import { WeatherService } from '../services/weather.service';
+import { WeatherDataBehaviorService } from '../../shared/services/weather-data-behavior.service';
 
 @Component({
   selector: 'app-form-weather',
@@ -8,23 +9,33 @@ import { WeatherService } from '../services/weather.service';
   styleUrl: './form-weather.component.scss',
 })
 export class FormWeatherComponent {
+  city: string = '';
   weather: Weather[] = [];
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(
+    private weatherService: WeatherService,
+    private weatherData: WeatherDataBehaviorService
+  ) {}
 
-  ngOnInit() {
-    console.log(this.getWeatherByCity('Sete Lagoas'));
+  ngOnInit() {}
+
+  getAndSetWeather() {
+    this.getWeatherByCity(this.city);
   }
 
-  getWeatherByCity(city: string) {
+  getWeatherByCity(city: string): Weather[] {
     this.weatherService.getWeatherByCity(city).subscribe({
       next: (weather) => {
         this.weather = [weather];
+        this.sendDataToBehaviorSubject();
       },
       error: (error) => {
         console.error(error + 'Erro ao buscar a previsão do tempo');
       },
     });
     return this.weather;
+  }
+  sendDataToBehaviorSubject() {
+    this.weatherData.changeData(this.weather[0]);
   }
 }
