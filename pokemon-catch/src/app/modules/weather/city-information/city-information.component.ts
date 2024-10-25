dce0bf57ca411ddebddd6e9c9395582548ed46b9
Sergio Felix <sergiofelix9@gmail.com>
2024-10-24 23:45:04 -0300

    refactor: Simplify city information component HTML and improve code readability

--- pokemon-catch/src/app/modules/weather/city-information/city-information.component.ts
+++ pokemon-catch/src/app/modules/weather/city-information/city-information.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherDataBehaviorService } from '../../../shared/services/weather-data-behavior.service';
 import { Weather } from '../../../shared/models/weather.model';
-import { isRainy } from '../../../shared/utils/functions';
+import {
+  convertFistLetterToUpperCase,
+  isRainy,
+} from '../../../shared/utils/functions';
 
 @Component({
   selector: 'city-information',
@@ -19,7 +22,36 @@ export class CityInformationComponent implements OnInit {
       },
     });
   }
-  getWeatherIcon(icon: any): string {
+
+  getWeatherIcon(): string | undefined {
+    return this.transformTheWeatherIconToUrl(
+      this.receivedWeather[0].weather?.[0]?.icon?.toString()
+    );
+  }
+
+  transformTheWeatherIconToUrl(icon: any): string {
     return `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  }
+
+  getWeatherCityName(): string | undefined {
+    return this.receivedWeather[0].name;
+  }
+
+  getWeatherCityCountry(): string | undefined {
+    return this.receivedWeather[0].sys?.country;
+  }
+
+  getWeatherCityTemperature(): number | undefined {
+    return this.receivedWeather[0].main?.temp;
+  }
+
+  getWeatherDescription(): string | undefined {
+    return convertFistLetterToUpperCase(
+      this.receivedWeather[0].weather?.[0]?.description || ''
+    );
+  }
+
+  getRainyStatus(): boolean {
+    return this.isRainy(this.receivedWeather[0].weather?.[0]?.main?.toString());
   }
 }
