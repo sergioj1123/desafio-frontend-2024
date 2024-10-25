426fc83bef7b85c070f06806f4bdc0e850faee68
Sergio Felix <sergiofelix9@gmail.com>
2024-10-24 23:22:15 -0300

    feat: Add utility functions for text manipulation

--- pokemon-catch/src/app/shared/utils/functions.ts
+++ pokemon-catch/src/app/shared/utils/functions.ts
@@ -1,3 +1,11 @@
 export function isRainy(rain: any): boolean {
   return rain === 'Rain' ? true : false;
 }
+
+export function changeHyphenToBlackSpace(text: string): string {
+  return text.replace('-', ' ');
+}
+
+export function convertFistLetterToUpperCase(text: string): string {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
